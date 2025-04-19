056f359028891842bbbfd7de7a6189112f577b81
Dillip Kumar Meher <dillipmj3@gmail.com>
2025-04-19 21:00:17 +0530

    Update Q03_sumOfAddNoInBetweenTheRange.java

--- p01_numberProgram/Q03_sumOfAddNoInBetweenTheRange.java
+++ p01_numberProgram/Q03_sumOfAddNoInBetweenTheRange.java
@@ -7,20 +7,14 @@ How:
 Run a loop from start to end.
 
 In each loop, check if the number is odd using i % 2 != 0.
-
 If it’s odd, add it to sum.
 
 Example (1 to 5):
 1 → odd → sum = 0+1 = 1
-
 2 → even
-
 3 → odd → sum = 1+3 = 4
-
 4 → even
-
 5 → odd → sum = 4+5 = 9
-
 Final sum = 9.*/
 
 public class Q03_sumOfAddNoInBetweenTheRange {
