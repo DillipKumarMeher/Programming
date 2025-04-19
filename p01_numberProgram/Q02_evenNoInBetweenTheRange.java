bc36fa008e907242eaba9cfbe9f8f5b9fcb492ff
Dillip Kumar Meher <dillipmj3@gmail.com>
2025-04-19 20:58:59 +0530

    Update Q02_evenNoInBetweenTheRange.java

--- p01_numberProgram/Q02_evenNoInBetweenTheRange.java
+++ p01_numberProgram/Q02_evenNoInBetweenTheRange.java
@@ -3,21 +3,15 @@ package p01_numberProgram;
 /*What this program does:
 
 It takes a start and end value.
-
 Runs a loop from start to end.
 
 In each loop, it checks if the number is even using i % 2 == 0.
-
 If yes, it prints that number.
 
 Example (1 to 10):
-
 1 → not even
-
 2 → even → print
-
 3 → not even
-
 4 → even → print
 ...and so on till 10.*/
 
